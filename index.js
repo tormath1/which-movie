b7da2b31cbb6164b27d385cabca8189f3c119cb6
Mathieu Tortuyaux <mathieu.tortuyaux@gmail.com>
2019-12-11 13:04:41 +0100

    index: return a promise for genreID

--- index.js
+++ index.js
@@ -6,29 +6,33 @@ var movie = {
 	// getGenreId makes a call to the API in order to
 	// fetch the category ID used by the API
 	getGenreId: (genre) => {
-		var options = {
-			headers: {
-				Authorization: 'Bearer ' + movie.token
-			}
-		}
-		https.get('https://api.themoviedb.org/3/genre/movie/list', options, (res) => {
-
-			res.on('data', (d) => {
-				var genres = JSON.parse(d).genres
-				for (var i = 0; i < genres.length; i ++) {
-					if (genres[i].name == genre) {
-						return genres[i].id
-					}
+		return new Promise((resolve, reject) => {
+			var options = {
+				headers: {
+					Authorization: 'Bearer ' + movie.token
 				}
-			});
+			}
+			https.get('https://api.themoviedb.org/3/genre/movie/list', options, (res) => {
 
-		}).on('error', (e) => {
-			// TODO: extract the error
-			console.error(e)
+				res.on('data', (d) => {
+					var genres = JSON.parse(d).genres
+					for (var i = 0; i < genres.length; i ++) {
+						if (genres[i].name == genre) {
+							resolve(genres[i].id)
+						}
+					}
+				});
+
+			}).on('error', (e) => {
+				// TODO: extract the error
+				console.error(e)
+				reject(e)
+			}).end();
 		});
 	}
 }
 
 movie.token = process.env.MOVIEDB_TOKEN
-console.log(movie.getGenreId('Action'))
-
+movie.getGenreId('Action').then((id) => {
+	console.log(id)
+})
